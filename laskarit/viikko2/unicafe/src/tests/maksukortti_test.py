4ea9ed03705b9585308fc5ff469657f5dda20093
Maarit Vilen <maarit.vilen@pp.inet.fi>
2023-03-27 20:11:58 +0300

    lisää testejä

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -15,4 +15,20 @@ class TestMaksukortti(unittest.TestCase):
 		self.maksukortti.lataa_rahaa(1000)
 
 		self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 20.00 euroa")
+	
+	def test_saldo_vahenee_oikein_jos_rahaa(self):
+		self.maksukortti.ota_rahaa(500)
+
+		self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 5.00 euroa")
+	
+	def test_saldo_ei_vahene_jos_ei_rahaa(self):
+		self.maksukortti.ota_rahaa(1500)
+
+		self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+	
+	def test_palauttaa_true(self):
+		self.maksukortti.ota_rahaa(500)==True
+	
+	def test_palauttaa_false(self):
+		self.maksukortti.ota_rahaa(1500)==False
 		
