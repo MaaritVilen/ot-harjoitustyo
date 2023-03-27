0f7dfd79a8fbdd444a179f886dba078de656a430
Maarit Vilen <maarit.vilen@pp.inet.fi>
2023-03-27 19:46:00 +0300

    uusia testeja

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -2,8 +2,17 @@ import unittest
 from maksukortti import Maksukortti
 
 class TestMaksukortti(unittest.TestCase):
-    def setUp(self):
-        self.maksukortti = Maksukortti(1000)
+	def setUp(self):
+        	self.maksukortti = Maksukortti(1000)
 
-    def test_luotu_kortti_on_olemassa(self):
-        self.assertNotEqual(self.maksukortti, None)
+	def test_luotu_kortti_on_olemassa(self):
+		self.assertNotEqual(self.maksukortti, None)
+
+	def test_kortin_saldo_alussa_oikein(self):
+		self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+	def test_rahan_lataus_oikein(self):
+		self.maksukortti.lataa_rahaa(1000)
+
+		self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 20.00 euroa")
+		
